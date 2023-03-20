4639ee6c19602012851cd360d6616012f5cb118b
Pedro Azevedo <pedrag1234@gmail.com>
2023-03-20 17:08:48 +0000

    Feat: Added function to heat CPU

--- cpu.py
+++ cpu.py
@@ -1,4 +1,6 @@
 import subprocess
+import multiprocessing
+import sys
 
 def cpu_temp() -> int:
     return int(subprocess.check_output(
@@ -10,3 +12,42 @@ def cpu_temperature() -> float:
     with open('/sys/class/thermal/thermal_zone0/temp', 'r') as file:
         cpu_temperature = float(file.read().strip()) / 1000
     return cpu_temperature
+
+
+def round_robin():
+    while(True):
+        number = 0
+        if(number >= sys.maxsize):
+            number = 0
+        else:
+            number = number + 1
+
+
+def heat_up_cpu() -> bool:
+    process_cnt = 1
+    processes = []
+    q = multiprocessing.Queue()
+
+    print("[CPU] - Spawning Processes to to Heat up CPU")
+    while(process_cnt <= multiprocessing.cpu_count() or cpu_temperature() > 60.0):
+        temp = multiprocessing.Process(target=round_robin)
+        processes.append(temp)
+        temp.start()
+        process_cnt += 1
+
+    print("[CPU] - Awaiting for spawned Processes to Finish or CPU temperature get high enough")
+    cpu_temp = cpu_temperature()
+    while (cpu_temp < 60.0):
+        cpu_temp = cpu_temperature()
+
+    for process in processes:
+        print("[CPU] - Terminating process")
+        process.terminate()
+
+        if not process.is_alive():
+            process.join(timeout=0.4)
+            print("[CPU] - Terminated process sucessfully joined")
+
+    print("[CPU] - Finished heating up cpu")
+    q.close()
+
